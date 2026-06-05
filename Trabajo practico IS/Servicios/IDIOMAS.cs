b2bb93c37e0e5f5704d0efb05b12085a5892f344
MatiuGramajo <MatiasFederico.Gramajo@alumnos.uai.edu.ar>
2026-06-05 17:03:55 -0300

    CLASE IDIOMAS EN LA CAPA DE SERVICIOS

---
+++ Trabajo practico IS/Servicios/IDIOMAS.cs
@@ -0,0 +1,61 @@
+﻿using BE;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Servicios
+{
+    public  class IDIOMAS: BE.IObservable
+    {
+
+        private static IDIOMAS _instancia;
+        private static object _lock = new object();
+        private List<BE.IObserver> _observadores = new List<BE.IObserver>();
+
+        private IDIOMAS() { }
+
+        public static IDIOMAS GetInstancia()
+        {
+            if(_instancia == null)
+            {
+                lock(_lock)
+                {
+                    if (_instancia == null) _instancia = new IDIOMAS();
+
+                }
+            }
+            return _instancia;
+        }
+
+        public void Desuscribir(IObserver observer)
+        {
+            if (!_observadores.Contains(observer))
+                _observadores.Add(observer);
+        }
+
+        public void Notificar(string mensaje="")
+        {
+            // Recorre todos los formularios suscritos y les avisa que se actualicen
+            foreach (var observer in _observadores)
+            {
+                observer.ActualizarIdioma();
+            }
+        }
+
+        public void Suscribir(IObserver observer)
+        {
+            if(_observadores.Contains(observer))
+                _observadores.Remove(observer);
+        }
+
+        public void CambiarIdioma(int nuevoIdIdioma)
+        {
+            // 1. Aquí irías a la BLL/DAL a buscar las nuevas traducciones para este Id.
+            // 2. Guardas el idioma actual.
+            // 3. Notificas a todos los formularios abiertos:
+            Notificar();
+        }
+    }
+}
